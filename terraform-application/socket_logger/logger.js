664cfd814d9521a4cb88eb6db8682dd9fdb62c2e
Hyeonmin Kim <khm970514@gmail.com>
2023-05-17 13:17:16 +0900

    update logger for terraform div

--- terraform-application/socket_logger/logger.js
+++ terraform-application/socket_logger/logger.js
@@ -12,3 +12,9 @@ exports.logging = function(data) {
     const msg = data.msg;
     socket.emit('log_health', msg + "\n");
 }
+
+exports.tfLogging = function(data) {
+    const socket = data.socket;
+    const msg = data.msg;
+    socket.emit('tf_log', msg + "<br>");
+}
