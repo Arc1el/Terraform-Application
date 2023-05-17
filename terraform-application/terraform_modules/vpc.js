3ba419275892ccb4df77e4278f5ff5b5af4ea71e
Hyeonmin Kim <khm970514@gmail.com>
2023-05-17 11:11:15 +0900

    refactored generate vpc code

--- terraform-application/terraform_modules/vpc.js
+++ terraform-application/terraform_modules/vpc.js
@@ -1,40 +1,34 @@
 exports.createVpc = function(data) {
-    const title = data.title;
-    const vpc_cidr = '"' + data.vpc_cidr + '"';
-    const pub = data.public_subnet_data;
-    const priv = data.private_subnet_data;
-    const db = data.database_subnet_data;
-    const nat = data.nat_gateway_data;
+    const access_key    = data.access_key;
+    const title         = data.title;
+    const vpc_cidr      = `"${data.vpc_cidr}"`;
+    const pub           = data.public_subnet_data;
+    const priv          = data.private_subnet_data;
+    const db            = data.database_subnet_data;
+    const nat           = data.nat_gateway_data;
 
-    console.log("nat gateway data : ", nat);
-
-    console.log("create vpc data received : ", title);
-
-    nat_context = "[";
+    let nat_context = "[";
     for (var i = 0; i <nat.length; i++) {
         let target_subnet = pub[nat[i]]; 
-        console.log("nat gateway target subnet :", target_subnet)
         nat_context += `"0${target_subnet.index + 1}.sub-${title}-prod-pub-${target_subnet.az}-01", ` 
     }
     nat_context += "]";
 
-    subnet_context = [];
-    for (var i = 0; i < pub.length; i++) {
+    let subnet_context = [];
+    for (var i = 0; i < pub.length; i++)
         subnet_context.push(`sub-${title}-prod-pub-${pub[i].az}-01", cidr = "${pub[i].cidr}", default_gateway = "${pub[i].gateway}"`)
-    }
-    for (var i = 0; i < priv.length; i++) {
+    for (var i = 0; i < priv.length; i++)
         subnet_context.push(`sub-${title}-prod-priv-${priv[i].az}-01", cidr = "${priv[i].cidr}", default_gateway = "${priv[i].gateway}"`)
-    }
-    for (var i = 0; i < db.length; i++) {
+    for (var i = 0; i < db.length; i++)
         subnet_context.push(`sub-${title}-prod-db-${db[i].az}-01", cidr = "${db[i].cidr}", default_gateway = "${db[i].gateway}"`)
-    }
+
+
     let subnet_len = subnet_context.length;
     let subnet_str = "subnet = [\n"
-    for (var i = 0; i < subnet_len; i++){
-        subnet_str += `            {name="0${i+1}.${subnet_context[i]}},\n`
-    }
+    for (var i = 0; i < subnet_len; i++)
+        subnet_str += `            {name="0${i+1}.${subnet_context[i]}},\n`;
     subnet_str+= "            ]\n"
-    console.log(subnet_str)
+
     try{
         var vpc_code = `module "vpc" {
     source = "git::ssh://git-codecommit.ap-northeast-2.amazonaws.com/v1/repos/terraform//modules//vpc"
@@ -48,7 +42,8 @@ exports.createVpc = function(data) {
     tags = { 
         Env  = "test"
     }
-}`
+}
+`
         return vpc_code;
     }catch(e){
         console.log(e);
